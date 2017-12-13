223cda57cfac1fbbb1d0b4c11c5ea6328e202ef4
xiaolv125 <15636331627@163.com>
2017-12-13 22:14:49 +0800

    小米商城-18

--- js/家电-选项卡.js
+++ js/家电-选项卡.js
@@ -4,5 +4,5 @@
 $('.tab-btns li').on('mouseover',function () {
     $(this).addClass('tab-active').siblings().removeClass('tab-active');
     var $idx=$(this).index();
-    $('.box-right ul').eq($idx).show().siblings().hide()
+    $(this).parent().siblings().children().find('ul').eq($idx).show().siblings().hide();
 })
